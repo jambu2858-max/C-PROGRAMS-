65e75bf2f002c47f6c49c0d37c7c0fcd99184d47
JAMBUNATH <jambu2858@gmail.com>
2025-11-18 08:54:37 +0530

    Create p2a.c

---
+++ p2a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int myInt = 42;
+    float myFloat = 3.14f;
+    char myChar = 'A';
+
+    printf("Integer value: %d\n", myInt);
+    printf("Float value: %f\n", myFloat);
+    printf("Character value: %c\n", myChar);
+
+    return 0;
+}

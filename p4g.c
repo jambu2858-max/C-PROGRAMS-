ddc2755d38c5b5880050370bc53df8a28da36c49
JAMBUNATH <jambu2858@gmail.com>
2025-11-18 19:58:26 +0530

    Create p4g.c

---
+++ p4g.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i = 1, sum = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        sum = sum + i;  
+        i++;             
+    }
+
+    printf("The sum of first %d natural numbers is: %d\n", n, sum);
+
+    return 0;
+}

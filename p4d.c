518b19d4714f9201965e1cba403559663e20d50a
JAMBUNATH <jambu2858@gmail.com>
2025-11-18 19:45:46 +0530

    Create p4d.c

---
+++ p4d.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > 0) {
+        if (b > 0) {
+            printf("Both numbers are positive.\n");
+        } else {
+            printf("First number is positive, second number is not positive.\n");
+        }
+    } else {
+        if (b > 0) {
+            printf("First number is not positive, second number is positive.\n");
+        } else {
+            printf("Both numbers are not positive.\n");
+        }
+    }
+
+    return 0;
+}

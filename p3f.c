5b19aef8b6e07c252de029bdfce17d1a384212a8
JAMBUNATH <jambu2858@gmail.com>
2025-11-18 19:19:10 +0530

    Create p3f.c

---
+++ p3f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, largest;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    largest = (a > b) ? a : b;
+
+    printf("The largest number is: %d\n", largest);
+
+    return 0;
+}
